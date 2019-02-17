d690d1a4ac47f420bf41668ab7b1f499f5ccb090
Mike Nguyen <nguyenduchung0609@gmail.com>
2019-02-17 17:46:18 +0800

    stateless function
    convert react class to function

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,11 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Title extends Component {
-    render() {
-        return (
-            <h1> {this.props.title} </h1>
-        )
-    }
+// class Title extends Component {
+//     render() {
+//         return (
+//             <h1> {this.props.title} </h1>
+//         )
+//     }
+// }
+function Title(props) {
+    return <h1> {props.title} </h1>
 }
 
 export default Title
